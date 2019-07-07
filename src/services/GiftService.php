48222ee9ace61d9a72c92d2175f7e593768b4162
Joe Moore <joe.yobo@gmail.com>
2019-07-07 18:56:04 +0100

    Added Logger

--- src/services/GiftService.php
+++ src/services/GiftService.php
@@ -2,11 +2,14 @@
 
 namespace App\services;
 
+use Psr\Log\LoggerInterface;
+
 class GiftService {
 public $gifts = ['flowers', 'car', 'piano', 'money'];
 
-public function __construct()
+public function __construct(LoggerInterface $logger)
 {
+    $logger->info('gifts were randomized');
     shuffle($this->gifts);
 }
 
